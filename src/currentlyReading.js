b6044d1cdc6e20fc34c90d0d2ecb220a32177179
A-Fredette <andrew.fredett@gmail.com>
2018-02-24 12:33:31 -0800

    initial commit

---
+++ src/currentlyReading.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class CurrentlyReading extends Component {
+
+  render () {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.booklist.map((book) => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.url})` }}></div>
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option onClick={() => this.props.moveToRead(book)} value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option onClick={() => this.props.removeBook(book)} value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.author}</div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CurrentlyReading
